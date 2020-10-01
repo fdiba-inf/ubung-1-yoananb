e7bae398fe5de994dcc3c73cb4b1415f7942cd66
yoananb <yoana_blagova@abv.bg>
2020-10-01 05:52:43 +0000

    nothing

---
+++ src/exercise1/Tree.java
@@ -0,0 +1,13 @@
+package exercise1;
+
+public class Tree{
+
+  public static void main (String[]args){
+    System.out.print("    *\n");
+    System.out.print("   ***\n");
+    System.out.print("  *****\n");
+    System.out.print(" *******\n");
+    System.out.print("*********\n");
+    System.out.print("   ***\n");
+  }
+}
